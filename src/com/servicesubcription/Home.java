6b35badf9adc90d4c42d2e073d5cd8be9819ebe6
gouravganguly <gourav1ganguly@gmail.com>
2020-04-02 04:37:41 +0100

    feat: edit customer, admin funcs

--- src/com/servicesubcription/Home.java
+++ src/com/servicesubcription/Home.java
@@ -36,6 +36,10 @@ public class Home extends JFrame {
 	TreeMapStorage tmap = new TreeMapStorage();
 	private JTextField textField_2;
 	private JTable table;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
 	/**
 	 * Launch the application.
 	 */
@@ -99,15 +103,12 @@ public class Home extends JFrame {
 		textField_2.setColumns(10);
 		
 		JButton btnSearch_1 = new JButton("Search");
+		
 		btnSearch_1.setBounds(108, 94, 89, 23);
 		panel_2.add(btnSearch_1);
 		
 		JButton btnDelete = new JButton("Delete");
-		btnDelete.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				tmap.deleteDetails(textField_2.getText());
-			}
-		});
+		
 		btnDelete.setBounds(212, 94, 89, 23);
 		panel_2.add(btnDelete);
 		
@@ -117,8 +118,18 @@ public class Home extends JFrame {
 		panel_2.add(lblAllUsers);
 		
 		JButton btnRefresh = new JButton("Refresh");
+		btnRefresh.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				table.setModel(tmap.getData());
+			}
+		});
 		btnRefresh.setBounds(362, 163, 89, 23);
 		panel_2.add(btnRefresh);
+		
+		JLabel lblAdminError = new JLabel("");
+		lblAdminError.setForeground(Color.RED);
+		lblAdminError.setBounds(108, 147, 193, 14);
+		panel_2.add(lblAdminError);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.desktop);
@@ -155,24 +166,21 @@ public class Home extends JFrame {
 		btnView.setBounds(124, 108, 89, 23);
 		panel_1.add(btnView);
 		
-		JButton btnSearch = new JButton("Search");
-		btnSearch.setBounds(223, 108, 89, 23);
-		panel_1.add(btnSearch);
-		
 		JButton btnBack = new JButton("Back");
 		
 		btnBack.setBounds(362, 0, 89, 23);
 		panel_1.add(btnBack);
 		
-		JLabel lblEdit = new JLabel("Edit");
-		lblEdit.setBounds(84, 142, 48, 14);
-		panel_1.add(lblEdit);
+		JButton btnNewButton_1 = new JButton("Edit User");
 		
-//		JPanel panel = new JPanel();
-//		panel.setBackground(Color.DARK_GRAY);
-//		layeredPane.add(panel, "name_885622712854300");
-//		panel.setLayout(null);
-//		
+		btnNewButton_1.setBounds(226, 108, 89, 23);
+		panel_1.add(btnNewButton_1);
+		
+		JLabel lblcustError = new JLabel("");
+		lblcustError.setForeground(Color.RED);
+		lblcustError.setBounds(124, 161, 229, 14);
+		panel_1.add(lblcustError);
+		
 		JLabel lblLogin = new JLabel("Home");
 		lblLogin.setForeground(SystemColor.inactiveCaptionBorder);
 		lblLogin.setBounds(198, 5, 71, 14);
@@ -185,6 +193,7 @@ public class Home extends JFrame {
 				layeredPane.add(panel_2);
 				layeredPane.repaint();
 				layeredPane.revalidate();
+				table.setModel(tmap.getData());
 			}
 		});
 		btnNewButton.setBounds(237, 76, 111, 23);
@@ -202,6 +211,114 @@ public class Home extends JFrame {
 		btnCustomer.setBounds(90, 76, 111, 23);
 		panel.add(btnCustomer);
 		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(SystemColor.controlHighlight);
+		layeredPane.add(panel_3, "name_14718061669900");
+		panel_3.setLayout(null);
+		
+		JLabel lblEditCustomer = new JLabel("Edit Customer");
+		lblEditCustomer.setBounds(191, 5, 115, 14);
+		panel_3.add(lblEditCustomer);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(SystemColor.desktop);
+		panel_4.setBounds(0, 38, 217, 148);
+		panel_3.add(panel_4);
+		panel_4.setLayout(null);
+		
+		JLabel lblEditUsername = new JLabel("Edit UserName");
+		lblEditUsername.setForeground(SystemColor.inactiveCaptionBorder);
+		lblEditUsername.setBounds(42, 5, 105, 14);
+		panel_4.add(lblEditUsername);
+		
+		JLabel lblUsername_1 = new JLabel("UserName");
+		lblUsername_1.setForeground(SystemColor.inactiveCaptionBorder);
+		lblUsername_1.setBounds(10, 33, 86, 14);
+		panel_4.add(lblUsername_1);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(91, 30, 96, 20);
+		panel_4.add(textField_3);
+		textField_3.setColumns(10);
+		textField_3.setEditable(false);
+		
+		JLabel lblNewUsername = new JLabel("New UserName");
+		lblNewUsername.setForeground(SystemColor.inactiveCaptionBorder);
+		lblNewUsername.setBounds(10, 58, 86, 14);
+		panel_4.add(lblNewUsername);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(91, 55, 96, 20);
+		panel_4.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JButton btnUpdate = new JButton("Update");
+		
+		btnUpdate.setBounds(91, 83, 96, 23);
+		panel_4.add(btnUpdate);
+		
+		JLabel lblEditUserError = new JLabel("");
+		lblEditUserError.setForeground(Color.RED);
+		lblEditUserError.setBounds(30, 123, 177, 14);
+		panel_4.add(lblEditUserError);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(SystemColor.desktop);
+		panel_5.setBounds(221, 38, 230, 148);
+		panel_3.add(panel_5);
+		panel_5.setLayout(null);
+		
+		JLabel lblEditSubscriptions = new JLabel("Delete Subscriptions");
+		lblEditSubscriptions.setForeground(SystemColor.inactiveCaptionBorder);
+		lblEditSubscriptions.setBounds(68, 5, 133, 14);
+		panel_5.add(lblEditSubscriptions);
+		
+		JLabel lblUsername_2 = new JLabel("UserName");
+		lblUsername_2.setForeground(SystemColor.inactiveCaptionBorder);
+		lblUsername_2.setBounds(10, 30, 73, 14);
+		panel_5.add(lblUsername_2);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(93, 27, 96, 20);
+		panel_5.add(textField_5);
+		textField_5.setColumns(10);
+		textField_5.setEditable(false);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(93, 52, 96, 20);
+		panel_5.add(textField_6);
+		textField_6.setColumns(10);
+		
+		JLabel lblSubsciption = new JLabel("Subsciption");
+		lblSubsciption.setForeground(SystemColor.inactiveCaptionBorder);
+		lblSubsciption.setBounds(10, 55, 73, 14);
+		panel_5.add(lblSubsciption);
+		
+		JButton btnDelete_1 = new JButton("Delete");
+		
+		btnDelete_1.setBounds(93, 83, 89, 23);
+		panel_5.add(btnDelete_1);
+		
+		JLabel lblEditCustError = new JLabel("");
+		lblEditCustError.setForeground(Color.RED);
+		lblEditCustError.setBounds(52, 123, 149, 14);
+		panel_5.add(lblEditCustError);
+		
+		JButton btnBack_2 = new JButton("Back");
+		btnBack_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layeredPane.removeAll();
+				layeredPane.add(panel_1);
+				layeredPane.repaint();
+				layeredPane.revalidate();
+				table.setModel(tmap.resetTable());
+				textField_4.setText("");
+				textField_6.setText("");
+			}
+		});
+		btnBack_2.setBounds(362, 1, 89, 23);
+		panel_3.add(btnBack_2);
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(36, 241, 441, 100);
 		contentPane.add(scrollPane);
@@ -212,10 +329,14 @@ public class Home extends JFrame {
 		
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+				textField_1.setText("");
+				lblcustError.setText("");
 				layeredPane.removeAll();
 				layeredPane.add(panel);
 				layeredPane.repaint();
 				layeredPane.revalidate();
+				table.setModel(tmap.resetTable());
 			}
 		});
 		btnBack_1.addActionListener(new ActionListener() {
@@ -224,16 +345,119 @@ public class Home extends JFrame {
 				layeredPane.add(panel);
 				layeredPane.repaint();
 				layeredPane.revalidate();
+				lblAdminError.setText("");
+				textField_2.setText("");
+				table.setModel(tmap.resetTable());
+				
 			}
 		});
 		
 		btnView.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				tmap.addDetails(textField.getText(), textField_1.getText());
-				table.setModel(tmap.getData());
-				
+				lblcustError.setText("");
+				if(textField.getText().isEmpty()) {
+					lblcustError.setText("UserName cannot be Empty");
+				} else if(textField_1.getText().isEmpty()) {
+					lblcustError.setText("Subscription cannot be Empty");
+				} else {
+					boolean isInserted = tmap.addDetails(textField.getText(), textField_1.getText());
+					if(isInserted) {
+						table.setModel(tmap.searchData(textField.getText()));
+						textField.setText("");
+						textField_1.setText("");
+					} else {
+						lblcustError.setText("Subscription already present");	
+					}
+					
+				}
 				
 			}
 		});		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblcustError.setText("");
+				if(!textField.getText().isEmpty()) {
+					if(tmap.checkUser(textField.getText())) {
+						layeredPane.removeAll();
+						layeredPane.add(panel_3);
+						layeredPane.repaint();
+						layeredPane.revalidate();
+						
+						textField_3.setText(textField.getText());
+						textField_5.setText(textField.getText());
+						table.setModel(tmap.searchData(textField.getText()));
+						textField.setText("");
+						textField_1.setText("");
+					} else {
+						lblcustError.setText("UserName not Present");
+					}
+				} else {
+					lblcustError.setText("UserName cannot be Empty");	
+				}	
+			}
+		});
+		btnDelete_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblEditCustError.setText("");
+				boolean deleted = tmap.deleteSubscription(textField_5.getText(), textField_6.getText());
+				if(deleted) {
+					table.setModel(tmap.searchData(textField_5.getText()));
+					textField_5.setText("");
+					textField_6.setText("");
+				} else {
+					lblEditCustError.setText("Sub not present");
+				}
+			}
+		});
+		
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblEditCustError.setText("");
+				lblEditUserError.setText("");
+				if(textField_4.getText().isEmpty()) {	
+					lblEditUserError.setText("New UserName cannot be Empty");
+				} else {
+					tmap.editUserName(textField_3.getText(), textField_4.getText());
+					table.setModel(tmap.searchData(textField_4.getText()));
+					textField_3.setText(textField_4.getText());
+					textField_5.setText(textField_4.getText());
+					textField_4.setText("");
+				}
+			}
+		});
+		
+		btnSearch_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(textField_2.getText().isEmpty()) {
+					lblAdminError.setText("UserName cannot be Empty");
+				} 
+				if(tmap.checkUser(textField_2.getText())) {
+					table.setModel(tmap.searchData(textField_2.getText()));
+					textField_2.setText("");
+					lblAdminError.setText("");
+				} else 
+					lblAdminError.setText("UserName not present");
+				
+			}
+		});
+		
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(textField_2.getText().isEmpty()) {
+					lblAdminError.setText("UserName cannot be Empty");
+				} else {
+					LinkedHashSet<String> val = tmap.deleteDetails(textField_2.getText());
+					if(val==null) {
+						lblAdminError.setText("UserName not present");
+					} else {
+						table.setModel(tmap.getData());
+						lblAdminError.setText("");
+					}
+				}
+				
+				
+			}
+		});
 	}
 }
--- src/com/servicesubcription/TreeMapStorage.java
+++ src/com/servicesubcription/TreeMapStorage.java
@@ -13,24 +13,40 @@ public class TreeMapStorage {
 	
 	public static TreeMap<String, LinkedHashSet<String>> tmap = new TreeMap<String, LinkedHashSet<String>>();
 	
-	public void addDetails(String name,String sub) {
+	public boolean addDetails(String name,String sub) {
 		
+		boolean isInserted = false;
 		LinkedHashSet<String> val = tmap.get(name);
 		if(val == null) {
-			LinkedHashSet<String> first = new LinkedHashSet<String>();
-			first.add(sub);
-			tmap.put(name, first);
+			val = new LinkedHashSet<String>();
+			isInserted = val.add(sub);
+			tmap.put(name, val);
 		} else {
-			val.add(sub);
+			isInserted = val.add(sub);
 			tmap.put(name, val);
 		}
 		for(Map.Entry m : tmap.entrySet()) {
 			System.out.println("Key: "+m.getKey()+" Value "+m.getValue());
-		}	
+		}
+		return isInserted;
 	}
-	public void deleteDetails(String name) {
-		tmap.remove(name);
+	public LinkedHashSet<String> deleteDetails(String name) {
+		  return tmap.remove(name);
 	}
+	public void editUserName(String oldName, String newName) {
+		LinkedHashSet<String> list = tmap.remove(oldName);
+		tmap.put(newName, list);
+	}
+	public boolean deleteSubscription(String userName, String subName) {
+		LinkedHashSet<String> allsubs = tmap.get(userName);
+		boolean deleted = allsubs.remove(subName);
+		tmap.put(userName, allsubs);
+		return deleted;
+	}
+	public boolean checkUser(String userName) {
+		return tmap.containsKey(userName);
+	}
+	
 	public TableModel getData() {
 		DefaultTableModel model = new DefaultTableModel(new Object[] { "UserName", "Subscriptions" },
 				0);
@@ -38,6 +54,20 @@ public class TreeMapStorage {
 			model.addRow(new Object[] { entry.getKey(), entry.getValue()});
 		}
 
+		return model;
+	}
+	public TableModel searchData(String userName) {
+		DefaultTableModel model = new DefaultTableModel(new Object[] { "UserName", "Subscriptions" },
+				0);
+		for (Map.Entry<String, LinkedHashSet<String>> entry : tmap.entrySet()) {
+			if(entry.getKey().equals(userName))
+				model.addRow(new Object[] { entry.getKey(), entry.getValue()});
+		}
+		return model;
+	}
+	public TableModel resetTable() {
+		DefaultTableModel model = new DefaultTableModel();
+		model.setRowCount(0);
 		return model;
 	}
 	
